27b3dfe42fdeb5c354d62320006e6d24bd6d3f48
Hean Chhinling <heanchhinling@admins-MacBook-Pro.local>
2024-09-14 18:52:19 +0200

    Adding frontend design

--- frontend/carpooling/src/App.js
+++ frontend/carpooling/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div className="text-5xl font-bold">
-      Hello World
+        <Home />
     </div>
   );
 }
---
+++ frontend/carpooling/src/components/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Home = () => {
+    return (
+        <div>
+           <nav className="bg-gradient-to-r from-blue-500 to-indigo-400 p-4 shadow-lg">
+                <div className="container mx-auto">
+                    <div className="flex justify-between items-center">
+                        <h1 className="text-white text-3xl px-4 py-2 font-extrabold">
+                            CarPooling
+                        </h1>
+                        <div className="flex space-x-6">
+                            <Link to="/" className="text-white px-4 py-2 rounded-md text-lg font-bold hover:bg-blue-700 hover:shadow-md transition-all duration-200 ease-in-out">Hitchiking</Link>
+                            <Link to="/" className="text-white px-4 py-2 rounded-md text-lg font-bold hover:bg-blue-700 hover:shadow-md transition-all duration-200 ease-in-out">User Guide</Link>
+                            <Link to="/" className="text-white px-4 py-2 rounded-md text-lg font-bold hover:bg-blue-700 hover:shadow-md transition-all duration-200 ease-in-out">Sign In</Link>
+                            <Link to="/" className="text-white px-4 py-2 rounded-md text-lg font-bold hover:bg-blue-700 hover:shadow-md transition-all duration-200 ease-in-out">Sign Up</Link>
+                        </div>
+                    </div>
+                </div>
+           </nav>
+        </div>
+    )
+}
+
+export default Home;
